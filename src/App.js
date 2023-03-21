2d8c6f73c3e7ad8171ea91b70e7ebc8766da95e6
Johnny <johnny_s.o@hotmail.com>
2023-03-21 19:02:50 -0300

    Adicionando body ao App.JS e testando branch home-static

--- src/App.js
+++ src/App.js
@@ -5,9 +5,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <ul>
+        <li><a href="#" className="action">Home</a></li>
           <li><a href="#" className="action">SOBRE</a></li>
         </ul>
       </header>
+
+      <body>
+        Teste
+      </body>
     </div>
   );
 }
